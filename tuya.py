868c64935288c1a90e0d88af9e4ed0057f0e6479
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2024-04-18 16:34:09 +1000

    Uncommented mainWindow function

--- tuya.py
+++ tuya.py
@@ -13,35 +13,19 @@ def mainWindow(stdscr):
     WHITE_AND_BLACK = curses.color_pair(1)
     GREEN_AND_BLACK = curses.color_pair(2)
 
-def main(stdscr):
-    # Initialise color attributes
-    # curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
-    #
-    # # Define color constants
-    # BLUE_AND_BLACK = curses.color_pair(1)
-    #
-    # # Define variables
-    # width = 40
-    # height = 20
-    # title = " Kogan Air Conditioner "
-    # # Add static items of the window
-    # stdscr.attron(BLUE_AND_BLACK)
-    # rectangle(stdscr, 0, 0, height, width)
-    # stdscr.addstr(0, (width - len(title)) // 2, title)
-    # stdscr.attroff(BLUE_AND_BLACK)
-    #
-    # # Print other screen elements
-    # subtitle = ["Room", "Temperature"]
-    # for index, word in enumerate(subtitle):
-    #     stdscr.addstr(3 + index, (width - len(word)) // 2, word)
-    # openapi, BASE_URL = tuya_scripts.connectTuya()
-    # mainWindow(stdscr)
+    # Define variables
+    height, width = stdscr.getmaxyx()
+    title = " Kogan Air Conditioner "
+    # Add static items of the window
+    stdscr.attron(GREEN_AND_BLACK)
+    rectangle(stdscr, 0, 0, height, width)
+    stdscr.addstr(0, (width - len(title)) // 2, title)
+    stdscr.attroff(GREEN_AND_BLACK)
 
-    # Get the current temperature and current temperature unit
-    # curTemperature = tuya_scripts.getCurrentTemp(openapi, BASE_URL)
-    # curTempUnit = tuya_scripts.getTempUnit(openapi, BASE_URL).capitalize()
-    # curTempString = f"{curTemperature}º{curTempUnit}"
-    # stdscr.addstr(5, (40 - len(curTempString)) // 2, curTempString)
+    # Print other screen elements
+    subtitle = ["Room", "Temperature"]
+    for index, word in enumerate(subtitle):
+        stdscr.addstr(3 + index, (width - len(word)) // 2, word)
 
     win = curses.newwin(10, 38, 15, 1)
     win.addstr(0, 0, "This is a test")
