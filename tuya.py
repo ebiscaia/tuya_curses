71cc41176800cba0f6c226013aaac69a052d8762
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2024-04-18 16:33:25 +1000

    Modified and add color variables

--- tuya.py
+++ tuya.py
@@ -9,6 +9,9 @@ def mainWindow(stdscr):
     curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
 
+    # Define color constants
+    WHITE_AND_BLACK = curses.color_pair(1)
+    GREEN_AND_BLACK = curses.color_pair(2)
 
 def main(stdscr):
     # Initialise color attributes
