0f662dba51853869d71321f46cdc71d1af0aad34
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2024-04-19 17:48:06 +1000

    Defined a new color pair and its corresponding variable

--- tuya.py
+++ tuya.py
@@ -8,10 +8,12 @@ def mainWindow(stdscr, status):
     # Initialise color attributes
     curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)
 
     # Define color constants
     WHITE_AND_BLACK = curses.color_pair(1)
     GREEN_AND_BLACK = curses.color_pair(2)
+    YELLOW_AND_BLACK = curses.color_pair(3)
 
     # Define variables
     height, width = stdscr.getmaxyx()
