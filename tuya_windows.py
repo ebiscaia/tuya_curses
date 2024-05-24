a8bb5b844d408109e7e14f3ba6c9380563108767
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2024-05-24 18:48:34 +1000

    Cosmetic changes to the window

--- tuya_windows.py
+++ tuya_windows.py
@@ -4,17 +4,19 @@ import curses
 def windowTest(color):
     heightw, widthw = 10, 40
     start_y, start_x = (curses.LINES - heightw) // 2, (curses.COLS - widthw) // 2
+    winTitle = " Curses Window "
+
     win = curses.newwin(heightw, widthw, start_y, start_x)
     win.bkgd(" ", color)
     win.border()
 
     # Title and message in the new window
-    win.addstr(1, 1, "Curses Window", color | curses.A_BOLD)
-    win.addstr(3, 1, "You pressed ", color)
+    win.addstr(0, (widthw - len(winTitle)) // 2, winTitle, color | curses.A_BOLD)
+    win.addstr(2, 1, "You pressed ", color)
     win.addstr(
-        heightw - 2,
-        1,
-        "Press any key to return to the main window",
+        heightw - 1,
+        2,
+        " Press any key to leave the window ",
         color,
     )
     win.refresh()
