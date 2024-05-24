9fb30e90c1a6d3ea96c8b871f538d98d412a77ee
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2024-05-24 18:40:16 +1000

    Removed function as it was moved to another file

--- tuya.py
+++ tuya.py
@@ -3,29 +3,6 @@ import re
 import tuya_scripts
 
 
-def windowTest(color):
-    heightw, widthw = 10, 40
-    start_y, start_x = (curses.LINES - heightw) // 2, (curses.COLS - widthw) // 2
-    win = curses.newwin(heightw, widthw, start_y, start_x)
-    win.bkgd(" ", color)
-    win.border()
-
-    # Title and message in the new window
-    win.addstr(1, 1, "Curses Window", color | curses.A_BOLD)
-    win.addstr(3, 1, "You pressed ", color)
-    win.addstr(
-        heightw - 2,
-        1,
-        "Press any key to return to the main window",
-        color,
-    )
-    win.refresh()
-
-    # Wait for another key press to close the window
-    win.getch()
-    del win
-
-
 def mainWindow(stdscr, status):
     # Initialise color attributes
     curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
