09a262e1bbebab1c3d0280d70d6454b03f26762b
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2024-05-27 10:34:08 +1000

    Defined function selectTempUnitWindow

--- tuya_windows.py
+++ tuya_windows.py
@@ -298,3 +298,66 @@ def selectModeWindow(openapi, BASE_URL, color):
         activeLine %= len(statesList)
 
 
+def selectTempUnitWindow(openapi, BASE_URL, color):
+    heightw, widthw = 10, 40
+    start_y, start_x = (curses.LINES - heightw) // 2, (curses.COLS - widthw) // 2
+    winTitle = " Select Oscilation Mode "
+
+    statesList = ["Celsius", "Fahrenheit"]
+    activeLine = 0
+
+    win = curses.newwin(heightw, widthw, start_y, start_x)
+    win.nodelay(True)
+    win.keypad(True)
+
+    # Add window loop
+
+    while True:
+        try:
+            key = win.getkey()
+        except:
+            key = None
+
+        # Start Window
+        win.clear()
+        win.bkgd(" ", color)
+        win.border()
+
+        # Title and message in the new window
+        win.addstr(0, (widthw - len(winTitle)) // 2, winTitle, color | curses.A_BOLD)
+
+        for index, state in enumerate(statesList):
+            spaceCount = widthw - (len(state) + 4)
+            win.addstr(2 + index, 2, state + spaceCount * " ", color)
+            if index == activeLine:
+                win.addstr(
+                    2 + index, 2, state + spaceCount * " ", color | curses.A_REVERSE
+                )
+
+        win.addstr(
+            heightw - 1,
+            2,
+            " Press any key to leave the window ",
+            color,
+        )
+        win.refresh()
+        curses.napms(100)
+
+        if key == chr(10):
+            command = tuya_scripts.setTempUnit(openapi, BASE_URL, activeLine + 1)
+            tuya_scripts.applyCommand(openapi, BASE_URL, command)
+            key = chr(27)
+
+        # Wait for another key press to close the window
+        if key == chr(27):
+            del win
+            break
+
+        # Scroll through the options
+        if key == "KEY_DOWN":
+            activeLine += 1
+        if key == "KEY_UP":
+            activeLine -= 1
+
+        # Control the variable so that highlight will not disappear
+        activeLine %= len(statesList)
